1cc5948fed59df200c2d6e6dfeff2ec88b3bb409
M Saif Ullah <chsaif369@gmail.com>
2026-01-05 21:08:56 +0500

    import fix

--- app/src/main/java/com/u4universe/composeplayground/movielist/compose/MainComposeActivity.kt
+++ app/src/main/java/com/u4universe/composeplayground/movielist/compose/MainComposeActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.u4universe.composeplayground.movielist.MoviesListViewModel
 import com.u4universe.composeplayground.ui.theme.ComposePlaygroundTheme
 
